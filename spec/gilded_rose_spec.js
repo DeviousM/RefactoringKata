c906fa62a9df73e1b98bedd4824bdc16868cb346
Michał Kostrzyński <michal.kostrzynski@vml.com>
2018-10-05 20:44:30 +0200

    Initial (and final I guess) commit.

---
+++ spec/gilded_rose_spec.js
@@ -0,0 +1,264 @@
+describe("Gilded Rose", function() {
+
+  describe("item's name should not change", function() {
+    const gildedRose = new Shop([new Item('+ 5 Dexterity Vest', 10, 20)]);
+    const items = gildedRose.updateQuality();
+
+    it("+ 5 Dexteriety Vest name did not change", function () {
+      expect(items[0].name).toEqual('+ 5 Dexterity Vest');
+    });
+  });
+
+  describe('should have natural degradation for regular item', function() {
+    const gildedRose = new Shop([new Item('+ 5 Dexterity Vest', 10, 20)]);
+    const items = gildedRose.updateQuality();
+    
+    it("+ 5 Dexteriety Vest should have sellIn as 9 and quality as 19", function() {
+      expect(items[0].sellIn).toEqual(9);
+      expect(items[0].quality).toEqual(19);
+    });
+  });
+
+  describe("should have twice as fast degredation for expired items", function() {
+    const gildedRose = new Shop([new Item('+ 5 Dexterity Vest', -1, 20)]);
+    const items = gildedRose.updateQuality();
+
+    it("+ 5 Dexteriety Vest should have sellIn as -2 and quality as 18", function () {
+      expect(items[0].sellIn).toEqual(-2);
+      expect(items[0].quality).toEqual(18);
+    });
+  });
+  
+  describe("should have twice as fast degredation for Conjured items", function() {
+    const gildedRose = new Shop([new Item('Conjured + 5 Dexterity Vest', 2, 20)]);
+    const items = gildedRose.updateQuality();
+
+    it("Conjured + 5 Dexteriety Vest should have sellIn as 1 and quality as 18", function () {
+      expect(items[0].sellIn).toEqual(1);
+      expect(items[0].quality).toEqual(18);
+    });
+  });
+
+  describe("should have quadruple as fast degredation for Conjured expired items", function() {
+    const gildedRose = new Shop([new Item('Conjured + 5 Dexterity Vest', -1, 20)]);
+    const items = gildedRose.updateQuality();
+
+    it("Conjured + 5 Dexteriety Vest should have sellIn as -2 and quality as 16", function () {
+      expect(items[0].sellIn).toEqual(-2);
+      expect(items[0].quality).toEqual(16);
+    });
+  });
+  
+  describe("item's quality cannot fall below 0", function() {
+    const gildedRose = new Shop([
+      new Item('+ 5 Dexterity Vest', -1, 0)
+    ]);
+    const items = gildedRose.updateQuality();
+
+    it("+ 5 Dexteriety Vest should have sellIn as -2 and quality as 0", function () {
+      expect(items[0].sellIn).toEqual(-2);
+      expect(items[0].quality).toEqual(0);
+    });
+  });
+  
+  describe("item's quality cannot rise above 50", function() {
+    const gildedRose = new Shop([
+      new Item('Aged Brie', 3, 50)
+    ]);
+    const items = gildedRose.updateQuality();
+
+    it("Aged Brie should have sellIn as 2 and quality as 50", function () {
+      expect(items[0].sellIn).toEqual(2);
+      expect(items[0].quality).toEqual(50);
+    });
+  });
+  
+  describe("Aged Brie should increase in quality with time", function() {
+    const gildedRose = new Shop([
+      new Item('Aged Brie', 2, 2),
+      new Item('Aged Brie', -1, 2)
+    ]);
+    const items = gildedRose.updateQuality();
+
+    it("Aged Brie should have sellIn as 1 and quality as 3", function () {
+      expect(items[0].sellIn).toEqual(1);
+      expect(items[0].quality).toEqual(3);
+    });
+    
+    it("Aged Brie should have sellIn as -2 and quality as 4 (sellIn < 0 = double quality increase)", function () {
+      expect(items[1].sellIn).toEqual(-2);
+      expect(items[1].quality).toEqual(4);
+    });
+  });
+  
+  describe("Legendary item should not change (quality should stay at 80)", function() {
+    const gildedRose = new Shop([
+      new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+      new Item('Sulfuras, Hand of Ragnaros', -1, 80)
+    ]);
+    const items = gildedRose.updateQuality();
+
+    it("Sulfuras, Hand of Ragnaros should have sellIn as 0 and quality as 80", function () {
+      expect(items[0].sellIn).toEqual(0);
+      expect(items[0].quality).toEqual(80);
+    });
+    
+    it("Sulfuras, Hand of Ragnaros should have sellIn as -1 and quality as 80", function () {
+      expect(items[1].sellIn).toEqual(-1);
+      expect(items[1].quality).toEqual(80);
+    });
+  });
+  
+  describe("Backstage Passes should increase in quality with time until expired", function() {
+    const gildedRose = new Shop([
+      new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 10, 43),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 5, 44),
+      new Item('Backstage passes to a TAFKAL80ETC concert', -1, 40)
+    ]);
+    const items = gildedRose.updateQuality();
+
+    it("Backstage passes to a TAFKAL80ETC concert should have sellIn as 14 and quality as 21", function () {
+      expect(items[0].sellIn).toEqual(14);
+      expect(items[0].quality).toEqual(21);
+    });
+    
+    it("Backstage passes to a TAFKAL80ETC concert should have sellIn as 9 and quality as 45", function () {
+      expect(items[1].sellIn).toEqual(9);
+      expect(items[1].quality).toEqual(45);
+    });
+    
+    it("Backstage passes to a TAFKAL80ETC concert should have sellIn as 4 and quality as 47", function () {
+      expect(items[2].sellIn).toEqual(4);
+      expect(items[2].quality).toEqual(47);
+    });
+
+    it("Backstage passes to a TAFKAL80ETC concert should have sellIn as -2 and quality as 0", function () {
+      expect(items[3].sellIn).toEqual(-2);
+      expect(items[3].quality).toEqual(0);
+    });
+  });
+
+  describe("should support multiple items", function() {
+    const multipleItems = [
+      new Item('Aged Brie', 2, 0),
+      new Item('Elixir of the Mongoose', 5, 7),
+      new Item('Sulfuras, Hand of Ragnaros', 0, 80)
+    ];
+
+    const gildedRose = new Shop(multipleItems);
+    const items = gildedRose.updateQuality();
+
+    it("should have Aged Brie sellIn as 1 and quality as 1", function () {
+      expect(items[0].name).toEqual('Aged Brie');
+      expect(items[0].sellIn).toEqual(1);
+      expect(items[0].quality).toEqual(1);
+    });
+
+    it("should have Elixir of the Mongoose sellIn as 4 and quality as 6", function () {
+      expect(items[1].name).toEqual('Elixir of the Mongoose');
+      expect(items[1].sellIn).toEqual(4);
+      expect(items[1].quality).toEqual(6);
+    });
+
+    it("should have Sulfuras, Hand of Ragnaros sellIn as 0 and quality as 80", function () {
+      expect(items[2].name).toEqual('Sulfuras, Hand of Ragnaros');
+      expect(items[2].sellIn).toEqual(0);
+      expect(items[2].quality).toEqual(80);
+    });
+  });
+
+  describe('helper Functions', function () {
+    const gildedRose = new Shop([
+      new Item('Conjured Aged Brie', -1, 0),
+      new Item('Sulfuras, Hand of Ragnaros', 2, 80),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 5, 44),
+      new Item('Elixir of the Mongoose', -1, 0),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 15, 19),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 10, 41),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 5, 41),
+      new Item('Backstage passes to a TAFKAL80ETC concert', -1, 40)
+    ]);
+    const items = gildedRose.updateQuality();
+
+    it('hasSellTimePassed is true when sellIn < 0', function () {
+      expect(hasSellTimePassed(items[0])).toEqual(true);
+    });
+
+    it('hasSellTimePassed is false when sellIn > 0', function () {
+      expect(hasSellTimePassed(items[1])).toEqual(false);
+    });
+
+    it('isLegendaryItem is true if name contains "Sulfuras"', function () {
+      expect(isLegendaryItem(items[1])).toEqual(true);
+    });
+
+    it('isLegendaryItem is false if name does not contain "Sulfuras"', function () {
+      expect(isLegendaryItem(items[0])).toEqual(false);
+    });
+
+    it('isAgedBrie is true if Aged Brie', function () {
+      expect(isAgedBrie(items[0])).toEqual(true);
+    });
+
+    it('isAgedBrie is false if not Aged Brie', function () {
+      expect(isAgedBrie(items[1])).toEqual(false);
+    });
+
+    it('isConjuredItem is true if name contains "Conjured"', function () {
+      expect(isConjuredItem(items[0])).toEqual(true);
+    });
+
+    it('isConjuredItem is false if name does not contain "Conjured"', function () {
+      expect(isConjuredItem(items[1])).toEqual(false);
+    });
+
+    it('isBackstagePass is true if name contains "Backstage passes"', function () {
+      expect(isBackstagePass(items[2])).toEqual(true);
+    });
+
+    it('isBackstagePass is false if name does not contain "Backstage passes"', function () {
+      expect(isBackstagePass(items[0])).toEqual(false);
+    });
+
+    it('computeQualityDegradationMultiplier returns 1 if item sellIn > 0 and is not conjured', function () {
+      expect(computeQualityDegradationMultiplier(items[2])).toEqual(1);
+    });
+    
+    it('computeQualityDegradationMultiplier returns 2 if item sellIn < 0', function () {
+      expect(computeQualityDegradationMultiplier(items[3])).toEqual(2);
+    });
+
+    it('computeQualityDegradationMultiplier returns 4 if item sellIn < 0 and is conjured', function () {
+      expect(computeQualityDegradationMultiplier(items[0])).toEqual(4);
+    });
+
+    it('normalizeQuality returns 0 if supplided value < 0', function () {
+      expect(normalizeQuality(-3)).toEqual(0);
+    });
+
+    it('normalizeQuality returns 50 if supplided value > 50', function () {
+      expect(normalizeQuality(60)).toEqual(50);
+    });
+
+    it('normalizeQuality returns value if supplided value > 0 and value < 50 ', function () {
+      expect(normalizeQuality(36)).toEqual(36);
+    });
+
+    it("computeBackstagePassQuality returns value increased by 1 if sellIn > 10", function () {
+      expect(computeBackstagePassQuality(items[4])).toEqual(21);
+    });
+    
+    it("computeBackstagePassQuality returns value increased by 2 if sellIn < 10 and sellIn > 5", function () {
+      expect(computeBackstagePassQuality(items[5])).toEqual(45);
+    });
+    
+    it("computeBackstagePassQuality returns value increased by 3 if sellIn < 5 and sellIn > 0", function () {
+      expect(computeBackstagePassQuality(items[6])).toEqual(47);
+    });
+    
+    it("computeBackstagePassQuality returns 0 if  sellIn < 0", function () {
+      expect(computeBackstagePassQuality(items[7])).toEqual(0);
+    });
+  });
+});
---
+++ src/gilded_rose.js
@@ -0,0 +1,143 @@
+class Item {
+  constructor(name, sellIn, quality){
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+}
+
+const MAX_QUALITY = 50;
+const MIN_QUALITY = 0;
+
+/** 
+ * @param {Item} item
+ * @returns {boolean}
+ * Returns true if `item.sellIn < 0`
+ */
+const hasSellTimePassed = (item) => (item.sellIn < 0);
+
+/** 
+ * @param {Item} item
+ * @returns {boolean}
+ * Returns true if name includes "Sulfuras"
+ */
+const isLegendaryItem = (item) => item.name.includes('Sulfuras');
+
+/** 
+ * @param {Item} item
+ * @returns {boolean}
+ * Returns true if name includes "Conjured"
+ */
+const isConjuredItem = (item) => item.name.includes('Conjured');
+
+/** 
+ * @param {Item} item
+ * @returns {boolean}
+ * Returns true if name includes "Backstage passes"
+ */
+const isBackstagePass = (item) => item.name.includes('Backstage passes');
+
+/** 
+ * @param {Item} item
+ * @returns {boolean}
+ * Returns true if name includes "Aged Brie"
+ */
+const isAgedBrie = (item) => item.name.includes('Aged Brie');
+
+/**
+ * @param {number} quality
+ * @returns {number}
+ * Returns normalized quality.
+ * Normal item's quality is always between 0 and 50.
+ */
+const normalizeQuality = (quality) => {
+  if (quality > MAX_QUALITY) return MAX_QUALITY;
+  if (quality < MIN_QUALITY) return MIN_QUALITY;
+  return quality;
+}
+
+/**
+ * @param {Item} item
+ * @returns {number}
+ * Returns computed quality degredation based on whether 
+ * item is conjured and if the sellIn date have passed.
+ */
+function computeQualityDegradationMultiplier(item) {
+  const sellDateMultiplier = !hasSellTimePassed(item) ? 1 : 2;
+  const conjuredMultiplier = !isConjuredItem(item) ? 1 : 2;
+  return sellDateMultiplier * conjuredMultiplier;
+};
+
+/**
+ * @param {Item} item
+ * @returns {number}
+ * Computes backstage pass quality based on sellIn days remaining.
+ */
+function computeBackstagePassQuality(item) {
+  const { sellIn, quality } = item;
+  if (hasSellTimePassed(item))
+    return MIN_QUALITY;
+  if (sellIn <= 5)
+    return quality + 3;
+  if (sellIn <= 10)
+    return quality + 2;
+  return quality + 1;
+}
+
+/* 
+  Has this been Typescript I'd have made all of Shop's internal 
+  methods private instead of putting an underscore.
+*/
+class Shop {
+  constructor(items = []) {
+    this.items = items;
+  }
+
+  /** 
+   * @param {Item} item
+   * @returns {number}
+   * Computes new item quality based on specific requirements
+   * Read more about requirements here: https://github.com/NotMyself/GildedRose
+   */
+  _computeNewQuality(item) {
+    if (isLegendaryItem(item)) return 80; // If item is legendary just return 80.
+
+    let { quality } = item;
+    const multiplier = computeQualityDegradationMultiplier(item);
+
+    if (isBackstagePass(item)) {
+      quality = computeBackstagePassQuality(item);
+    } else {
+      const sign = !isAgedBrie(item) ? -1 : 1;
+      quality = quality + sign * multiplier;
+    }
+
+    return normalizeQuality(quality);
+  }
+
+  /** 
+   * @param {Item} item
+   * @returns {number}
+   * Computes new sellIn time based on whether item is Legendary or not.
+   */
+  _computeNewSellInTime = (item) => !isLegendaryItem(item) ? item.sellIn - 1 : item.sellIn;
+
+  /**
+   * @param {Item} item
+   * Updates singular item
+   */
+  _updateItem = (item) => {
+    item.quality = this._computeNewQuality(item);
+    item.sellIn = this._computeNewSellInTime(item);
+  }
+
+  /**
+   * Method triggered at the end of the day.
+   * Updates all of the items in the shop.
+   */
+  updateQuality() {
+    this.items.forEach(this._updateItem);
+
+    return this.items;
+  }
+}
